ba7355080ea766822ec3c363ea7dd329f53e294c
Ciaran <ciaranhuang@vip.qq.com>
2023-08-25 14:49:37 +0800

    MutexHelper.cs

---
+++ code/CodeLib/CodeLib01/MutexHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+
+namespace CodeLib01;
+
+public static class MutexHelper
+{
+    public static Mutex WaitOne(string name)
+    {
+        var mutex = new Mutex(false, name);
+        try
+        {
+            mutex.WaitOne();
+        }
+        catch (AbandonedMutexException ex)
+        {
+        }
+
+        return mutex;
+    }
+
+    public static void ReleaseClose(this Mutex mutex)
+    {
+        try
+        {
+            mutex.ReleaseMutex();
+        }
+        catch
+        {
+        }
+
+        try
+        {
+            mutex.Close();
+        }
+        catch
+        {
+        }
+    }
+}
