5b833ef8a655af92df415ced8d8a0485a427fc26
Ciaran <ciaranhuang@vip.qq.com>
2023-08-30 16:47:12 +0800

    Base64 encode, decode

---
+++ code/CodeLib/CodeLib01/EncodeHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLib01
+{
+    public static class EncodeHelper
+    {
+
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="codeName">加密采用的编码方式</param>
+        /// <param name="source">待加密的明文</param>
+        /// <returns></returns>
+        public static string EncodeBase64(Encoding encode, string source)
+        {
+            string encodestr = "";
+            byte[] bytes = encode.GetBytes(source);
+            try
+            {
+                encodestr = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encodestr = source;
+            }
+            return encodestr;
+        }
+
+        /// <summary>
+        /// Base64加密，采用utf8编码方式加密
+        /// </summary>
+        /// <param name="source">待加密的明文</param>
+        /// <returns>加密后的字符串</returns>
+        public static string EncodeBase64(string source)
+        {
+            return EncodeBase64(Encoding.UTF8, source);
+        }
+
+    }
+}
