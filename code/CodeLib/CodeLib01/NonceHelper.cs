9ac18108c75e3c9002c9f6eb0db14aea8ce2cfc1
Ciaran <ciaranhuang@vip.qq.com>
2023-09-14 11:14:23 +0800

    nonceHelper, signHelper

---
+++ code/CodeLib/CodeLib01/NonceHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeLib01
+{
+    public static class NonceHelper
+    {
+
+        /// <summary>
+        /// 创建5字的请求随机key<br/>
+        /// 2023-1-11 Ciaran
+        /// </summary>
+        /// <returns></returns>
+        public static string Nonce5Char()
+        {
+            // 2023-1-11 Ciaran 生成新的Guid, 并通过guid的字节值计算得出对应随机字符
+            string rdKey = new string(Guid.NewGuid()
+                    .ToByteArray()
+                    .Period(3) // 16个字节 , 每3个为一组, 有6组
+                    .Take(5) // 取前5个
+                    .Select(list => ((list[2] << 16) + (list[1] << 8) + list[0]) % 36) // 得出当个字节映射
+                    .Select(i => i < 26 ? (char)('a' + i) : (char)('0' + i - 26)) // 映射为字符
+                    .ToArray()) // 字符数组 转 字符串
+                ;
+            return rdKey;
+        }
+
+        /// <summary>
+        /// newGuid.SubString(start, len);
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static string SubGuid(int start = 2, int len = 10) => Guid.NewGuid().ToString().Replace("-", "").Substring(start, len);
+    }
+}
---
+++ code/CodeLib/CodeLib01/SignHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CodeLib01
+{
+    public static class SignHelper
+    {
+
+        /// <summary>
+        /// 签名(MD5 + Base64)<br/>
+        /// 2023-9-14 Ciaran
+        /// </summary>
+        /// <param name="base_string">拼接字符串</param>
+        /// <returns></returns>
+        public static string MD5_Base64(string base_string)
+        {
+            byte[] md5data;
+            using (MD5 md5 = new MD5CryptoServiceProvider())
+            {
+                md5data = md5.ComputeHash(Encoding.UTF8.GetBytes(base_string));
+            }
+
+            return Convert.ToBase64String(md5data);
+        }
+
+        /// <summary>
+        /// 签名(MD5 + Base64)<br/>
+        /// 2023-9-14 Ciaran
+        /// </summary>
+        /// <param name="base_string">拼接字符串</param>
+        /// <returns></returns>
+        public static string MD5_Base64_2(string base_string)
+        {
+            byte[] md5data;
+            using (MD5 md5 = new MD5CryptoServiceProvider())
+            {
+                md5data = md5.ComputeHash(Encoding.UTF8.GetBytes(base_string));
+            }
+
+            md5data = Encoding.UTF8.GetBytes(string.Join("", md5data.Select(bt => bt.ToString("x2"))));
+            return Convert.ToBase64String(md5data);
+        }
+
+        /// <summary>
+        /// 签名(HmacSha256 + Base64)<br/>
+        /// 2023-9-14 Ciaran
+        /// </summary>
+        /// <param name="base_string">拼接字符串</param>
+        /// <param name="nonce_key">密匙 + 随机字符串</param>
+        /// <returns></returns>
+        public static string HmacSHA256_Base64(string base_string, string nonce_key)
+        {
+            byte[] hash;
+            using (HMACSHA256 mac = new HMACSHA256(Encoding.UTF8.GetBytes(nonce_key)))
+            {
+                hash = mac.ComputeHash(Encoding.UTF8.GetBytes(base_string));
+            }
+
+            return Convert.ToBase64String(hash);
+        }
+
+
+        /// <summary>
+        /// 签名(HmacSha256 + Base64)<br/>
+        /// 2023-9-14 Ciaran
+        /// </summary>
+        /// <param name="base_string">拼接字符串</param>
+        /// <param name="nonce_key">密匙 + 随机字符串</param>
+        /// <returns></returns>
+        public static string HmacSHA256_Base64_2(string base_string, string nonce_key)
+        {
+            byte[] hash;
+            using (HMACSHA256 mac = new HMACSHA256(Encoding.UTF8.GetBytes(nonce_key)))
+            {
+                hash = mac.ComputeHash(Encoding.UTF8.GetBytes(base_string));
+            }
+            hash = Encoding.UTF8.GetBytes(string.Join("", hash.Select(bt => bt.ToString("x2"))));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
