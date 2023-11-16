a1c0f63b73446f7f9db8ea06c7a74b7a291f7cba
Ciaran <ciaranhuang@vip.qq.com>
2023-11-16 16:05:59 +0800

    NameObjectCollectionHelper.cs NameValueCollectionHelper.cs

---
+++ code/CodeLib/CodeLib01/NameObjectCollectionHelper.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
+
+namespace CodeLib01;
+
+public static class NameObjectCollectionHelper
+{
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <returns></returns>
+    public static Dictionary<string, object> ToDictionary(this System.Collections.Specialized.NameObjectCollectionBase collection)
+    {
+        int count = collection.Count;
+        var dict = new Dictionary<string, object>(count);
+        foreach (var kv in collection.AsKeyValueEnumerable())
+        {
+            dict.Add(kv.Key, kv.Value);
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <returns></returns>
+    public static Dictionary<string, T> ToDictionary<T>(this System.Collections.Specialized.NameObjectCollectionBase collection)
+    {
+        int count = collection.Count;
+        var dict = new Dictionary<string, T>(count);
+        foreach (var kv in collection.AsKeyValueEnumerable())
+        {
+            dict.Add(kv.Key, (T)kv.Value);
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="format_value">格式化函数</param>
+    /// <returns></returns>
+    public static Dictionary<string, T> ToDictionary<T>(this System.Collections.Specialized.NameObjectCollectionBase collection, Func<object, T>? format_value)
+    {
+        if (format_value is null)
+        {
+            return ToDictionary<T>(collection);
+        }
+
+        int count = collection.Count;
+        var dict = new Dictionary<string, T>(count);
+        foreach (var kv in collection.AsKeyValueEnumerable())
+        {
+            dict.Add(kv.Key, format_value(kv.Value));
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="format_value">格式化函数</param>
+    /// <returns></returns>
+    public static Dictionary<string, T> ToDictionary<T>(this System.Collections.Specialized.NameObjectCollectionBase collection, Func<string, object, T>? format_value)
+    {
+        if (format_value is null)
+        {
+            return ToDictionary<T>(collection);
+        }
+
+        int count = collection.Count;
+        var dict = new Dictionary<string, T>(count);
+        foreach (var kv in collection.AsKeyValueEnumerable())
+        {
+            dict.Add(kv.Key, format_value(kv.Key, kv.Value));
+        }
+
+        return dict;
+    }
+
+    public static IEnumerable<object> AsValueEnumerable(this System.Collections.Specialized.NameObjectCollectionBase collection)
+    {
+        for (var e1 = new NameObjectValueEnumerator(collection); e1.MoveNext();)
+        {
+            yield return e1.Current;
+        }
+    }
+
+    public static IEnumerable<T> AsValueEnumerable<T>(this System.Collections.Specialized.NameObjectCollectionBase collection)
+    {
+        for (var e1 = new NameObjectValueEnumerator(collection); e1.MoveNext();)
+        {
+            yield return (T)e1.Current;
+        }
+    }
+
+    public static IEnumerable<KeyValuePair<string, object>> AsKeyValueEnumerable(this System.Collections.Specialized.NameObjectCollectionBase collection)
+    {
+        for (var e1 = new NameObjectEnumerator(collection); e1.MoveNext();)
+        {
+            yield return e1.Current;
+        }
+    }
+
+    public class NameObjectEnumerator : IEnumerator<KeyValuePair<string, object>>
+    {
+        System.Collections.IEnumerator key_enumerator;
+        int _pos = -1;
+        NameObjectCollectionBase _coll;
+
+        MethodInfo GetValueMethod;
+
+        public NameObjectEnumerator(NameObjectCollectionBase collectionBase)
+        {
+            this._coll = collectionBase;
+            key_enumerator = collectionBase.GetEnumerator();
+
+            GetValueMethod = typeof(NameObjectCollectionBase).GetMethod("BaseGet", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(int) }, null);
+            if (GetValueMethod == null)
+            {
+                throw new TargetException("NameObjectCollectionBase 中缺少BaseGet(int) 方法");
+            }
+        }
+
+        public KeyValuePair<string, object> Current => new KeyValuePair<string, object>((string)key_enumerator.Current, GetValueMethod.Invoke(this._coll, new object[] { _pos }));
+
+        object System.Collections.IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (!key_enumerator.MoveNext()) return false;
+            if (_pos < _coll.Count - 1)
+            {
+                _pos++;
+                return true;
+            }
+            else
+            {
+                _pos = _coll.Count;
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            key_enumerator.Reset();
+            _pos = -1;
+        }
+    }
+
+
+    public class NameObjectValueEnumerator : IEnumerator<object>
+    {
+        System.Collections.IEnumerator key_enumerator;
+        int _pos = -1;
+        NameObjectCollectionBase _coll;
+
+        MethodInfo GetValueMethod;
+
+        public NameObjectValueEnumerator(NameObjectCollectionBase collectionBase)
+        {
+            this._coll = collectionBase;
+            key_enumerator = collectionBase.GetEnumerator();
+
+            GetValueMethod = typeof(NameObjectCollectionBase).GetMethod("BaseGet", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(int) }, null);
+            if (GetValueMethod == null)
+            {
+                throw new TargetException("NameObjectCollectionBase 中缺少BaseGet(int) 方法");
+            }
+        }
+
+        public object Current => GetValueMethod.Invoke(this._coll, new object[] { _pos });
+
+        object System.Collections.IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (!key_enumerator.MoveNext()) return false;
+            if (_pos < _coll.Count - 1)
+            {
+                _pos++;
+                return true;
+            }
+            else
+            {
+                _pos = _coll.Count;
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            key_enumerator.Reset();
+            _pos = -1;
+        }
+    }
+}
---
+++ code/CodeLib/CodeLib01/NameValueCollectionHelper.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
+
+namespace CodeLib01;
+
+public static class NameValueCollectionExtension
+{
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <returns></returns>
+    public static Dictionary<string, string> ToDictionary(this System.Collections.Specialized.NameValueCollection collection)
+    {
+        int count = collection.Count;
+        var dict = new Dictionary<string, string>(count);
+        for (int i = 0; i < count; i++)
+        {
+            dict.Add(collection.GetKey(i), collection.Get(i));
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="format_value">格式化函数</param>
+    /// <returns></returns>
+    public static Dictionary<string, string> ToDictionary(this System.Collections.Specialized.NameValueCollection collection, Func<string, string>? format_value)
+    {
+        if (format_value is null)
+        {
+            return ToDictionary(collection);
+        }
+
+        int count = collection.Count;
+        var dict = new Dictionary<string, string>(count);
+        for (int i = 0; i < count; i++)
+        {
+            dict.Add(collection.GetKey(i), format_value(collection.Get(i)));
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// 转字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="format_value">格式化函数</param>
+    /// <returns></returns>
+    public static Dictionary<string, string> ToDictionary(this System.Collections.Specialized.NameValueCollection collection, Func<string, string, string>? format_value)
+    {
+        if (format_value is null)
+        {
+            return ToDictionary(collection);
+        }
+
+        int count = collection.Count;
+        var dict = new Dictionary<string, string>(count);
+        for (int i = 0; i < count; i++)
+        {
+            string key = collection.GetKey(i);
+            dict.Add(key, format_value(key, collection.Get(i)));
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// 转多值字典
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <returns></returns>
+    public static Dictionary<string, string[]> ToValuesDictionary(this System.Collections.Specialized.NameValueCollection collection)
+    {
+        int count = collection.Count;
+        var dict = new Dictionary<string, string[]>(count);
+        for (int i = 0; i < count; i++)
+        {
+            dict.Add(collection.GetKey(i), collection.GetValues(i));
+        }
+
+        return dict;
+    }
+
+    public static IEnumerable<KeyValuePair<string, string>> AsKeyValueEnumerable(this System.Collections.Specialized.NameValueCollection collection)
+    {
+        for (var e1 = new NameValueEnumerator(collection); e1.MoveNext();)
+        {
+            yield return e1.Current;
+        }
+    }
+
+    public static IEnumerable<KeyValuePair<string, string[]>> AsKeyValuesEnumerable(this System.Collections.Specialized.NameValueCollection collection)
+    {
+        for (var e1 = new NameValuesEnumerator(collection); e1.MoveNext();)
+        {
+            yield return e1.Current;
+        }
+    }
+
+    public static short? GetShort(this NameValueCollection that, string key)
+    {
+        return that[key]?.AsInt16();
+    }
+
+    public static int? GetInt(this NameValueCollection that, string key)
+    {
+        return that[key]?.AsInt32();
+    }
+
+    public static long? GetLong(this NameValueCollection that, string key)
+    {
+        return that[key]?.AsInt64();
+    }
+
+    public static double? GetDouble(this NameValueCollection that, string key)
+    {
+        return that[key]?.AsDouble();
+    }
+
+    public static decimal? GetDecimal(this NameValueCollection that, string key)
+    {
+        return that[key]?.AsDecimal();
+    }
+
+    public static DateTime? GetDateTime(this NameValueCollection that, string key)
+    {
+        return that[key]?.AsDateTime();
+    }
+
+    public class NameValueEnumerator : IEnumerator<KeyValuePair<string, string>>
+    {
+        System.Collections.IEnumerator key_enumerator;
+        int _pos = -1;
+        NameValueCollection _coll;
+
+        MethodInfo GetValueMethod;
+
+        public NameValueEnumerator(NameValueCollection collection)
+        {
+            this._coll = collection;
+            key_enumerator = collection.GetEnumerator();
+        }
+
+        public KeyValuePair<string, string> Current => new KeyValuePair<string, string>((string)key_enumerator.Current, this._coll.Get(_pos));
+
+        object System.Collections.IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (!key_enumerator.MoveNext()) return false;
+            if (_pos < _coll.Count - 1)
+            {
+                _pos++;
+                return true;
+            }
+            else
+            {
+                _pos = _coll.Count;
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            key_enumerator.Reset();
+            _pos = -1;
+        }
+    }
+
+    public class NameValuesEnumerator : IEnumerator<KeyValuePair<string, string[]>>
+    {
+        System.Collections.IEnumerator key_enumerator;
+        int _pos = -1;
+        NameValueCollection _coll;
+
+        MethodInfo GetValueMethod;
+
+        public NameValuesEnumerator(NameValueCollection collection)
+        {
+            this._coll = collection;
+            key_enumerator = collection.GetEnumerator();
+        }
+
+        public KeyValuePair<string, string[]> Current => new KeyValuePair<string, string[]>((string)key_enumerator.Current, this._coll.GetValues(_pos));
+
+        object System.Collections.IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (!key_enumerator.MoveNext()) return false;
+            if (_pos < _coll.Count - 1)
+            {
+                _pos++;
+                return true;
+            }
+            else
+            {
+                _pos = _coll.Count;
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            key_enumerator.Reset();
+            _pos = -1;
+        }
+    }
+}
